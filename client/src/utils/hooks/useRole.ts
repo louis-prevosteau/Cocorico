fed0d78a85a8f28ae934e981184a70ad08b304ab
Louis Prevosteau <louis.prevosteau@laposte.net>
2024-12-22 08:35:23 +0100

    `Removed GitHub Actions workflows for Prettier and Cocorico Deploy, updated AllowedRoutes component to accept array of roles, and modified useRole hook to accept array of roles.`

--- client/src/utils/hooks/useRole.ts
+++ client/src/utils/hooks/useRole.ts
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
 import { RootState } from 'redux/Store';
 
-export const useRole = (role: string) => {
+export const useRole = (roles: string[]) => {
     const { profile } = useSelector((state: RootState) => state);
 
-    return profile.roles?.includes(role);
+    return roles.some((role) => profile.roles?.includes(role));
 };
--- client/src/utils/routes/AllowedRoutes.tsx
+++ client/src/utils/routes/AllowedRoutes.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useRole } from 'utils/hooks';
 
-export const AllowedRoutes = ({ role }: { role: string }) => {
-    return useRole(role) ? <Outlet /> : <Navigate to={'/auth'} />;
+export const AllowedRoutes = ({ roles }: { roles: string[] }) => {
+    return useRole(roles) ? <Outlet /> : <Navigate to={'/auth'} />;
 };
